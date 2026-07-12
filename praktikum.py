8097c17c0a337f7ac1868bc2c20b2b79837bba29
Admin <mr.xarifmaca@gmail.com>
2026-07-12 21:46:46 +0700

    Memperbaiki Segmentation Fault di Streamlit Cloud dengan memaksa matplotlib menggunakan backend Agg (Headless)

--- praktikum.py
+++ praktikum.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg') # FIX SEGFAULT: Gunakan backend non-GUI (Agg) di server
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
